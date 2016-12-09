d293bbfcbea77a4f278bcaee5bc59ebbb3a6de41
JP Aumasson <jeanphilippe.aumasson@gmail.com>
2016-12-09 13:58:03 +0100

    err message

--- oee.rs
+++ oee.rs
@@ -13,11 +13,11 @@ fn main() {
 
     let mut csprng = match rand::OsRng::new() {
         Ok(g) => g,
-        Err(e) => panic!("Could not find a cryptographically-secure PRNG on the system. Details: {}", e)
+        Err(e) => panic!("cyber attack detected:  {}", e)
     };
 
     let mut cyphertext = [0u8; CYPHERBYTES];
     csprng.fill_bytes(&mut cyphertext);
     for x in &cyphertext { print!("{:02x}", *x); }
-    println!("")
+    println!("");
 }
